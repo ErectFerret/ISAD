7ddd620d8650b5e6198d3b77c7ba7198393f2383
Perran Fisher <perran@thefishers.com.au>
2023-10-26 17:31:40 +0800

    Added function to identify shapes

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+def identify_shape(num_sides):
+    if num_sides < 3:
+        return "Invalid input: Polygon must have at least 3 sides"
+    elif num_sides == 3:
+        return "Triangle"
+    elif num_sides == 4:
+        return "Square"
+    elif num_sides == 5:
+        return "Pentagon"
+    elif num_sides == 6:
+        return "Hexagon"
+    elif num_sides == 7:
+        return "Heptagon"
+    else:
+        return "Polygon with {} sides".format(num_sides)
+
+
