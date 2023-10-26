d7222300f4b6d73c3252812a30f5f8ee9a09b9df
Perran Fisher <perran@thefishers.com.au>
2023-10-26 17:44:01 +0800

    Added function to visualize shapes with turtle library

--- main.py
+++ main.py
@@ -15,5 +15,15 @@ def identify_shape(num_sides):
         return "Heptagon"
     else:
         return "Polygon with {} sides".format(num_sides)
-
-
+    
+def visualize_shape(num_sides):
+    if num_sides < 3:
+        print("Invalid input: Polygon must have at least 3 sides")
+    else:
+        window = turtle.Screen()
+        t = turtle.Turtle()
+        angle = 360 / num_sides
+        for _ in range(num_sides):
+            t.forward(100)  # Adjust the length as needed
+            t.right(angle)
+        window.exitonclick()
