7ece141bc4e1f75b7e5318cd6c1d8d19f5c42673
Perran Fisher <perran@thefishers.com.au>
2023-10-28 14:08:02 +0800

    Added test case 2

--- whitebox.py
+++ whitebox.py
@@ -7,3 +7,12 @@ def test_identify_shape_valid_shapes():
     for sides in valid_sides:
         assert identify_shape(sides) in ["Triangle", "Square", "Pentagon", "Hexagon", "Heptagon"]
 
+def test_identify_shape_invalid_input():
+    # Test Case 2
+    invalid_sides = [-1, 0, 2, 8, 10]
+    for sides in invalid_sides:
+        try:
+            identify_shape(sides)
+            assert False, f"No exception raised for invalid side: {sides}"
+        except ValueError:
+            pass  # Expecting a ValueError
