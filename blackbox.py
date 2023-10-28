7de5ce0bf68cd146e11a97a9d6a95563b90b09fc
Perran Fisher <perran@thefishers.com.au>
2023-10-28 12:57:55 +0800

    Updated in progress blackbox.py

--- blackbox.py
+++ blackbox.py
@@ -2,3 +2,11 @@ import turtle
 from main import identify_shape
 
 def test_identify_shape():
+    # Test valid shapes
+    assert identify_shape(5) == "Pentagon"  # Partition 1
+    assert identify_shape(3) == "Triangle"  # Partition 2
+    assert identify_shape(7) == "Heptagon"  # Partition 3
+
+    # Test invalid shapes
+    try:
+        identify_shape(4)  # Partition 2: Invalid number of sides (< 3)
